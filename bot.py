59789dbb7e90d33ec450443c4a2c06d742a3ef48
Tan Yiming <tanyiming.nus@gmail.com>
2023-12-24 22:38:43 +0800

    Create a basic echo bot

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TOKEN).build()
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
